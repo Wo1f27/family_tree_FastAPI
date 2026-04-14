13bd47993a473c0ec05ff8027fd7fda85f7b39e5
Aleksandr Strelkov <a.strelkov@aitiguru.com>
2026-04-14 18:54:30 +0300

    feat: add init for entities

---
+++ core/domain/entities/__init__.py
@@ -0,0 +1,5 @@
+from core.domain.entities.person import Person
+from core.domain.entities.user import User
+from core.domain.entities.relationship import Relationship
+
+__all__ = ['Person', 'User', 'Relationship']
